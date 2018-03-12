202e42999de09d417afa2e35450b8d65cc46dce0
Alex Pham <alex.pham2010@gmail.com>
2018-03-13 00:03:38 +1100

    update README

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -5,7 +5,7 @@ import * as routes from '../constants/routes';
 
 const LandingPage = () =>
   <div className="music-landing-container">
-    <h1 className="music-landing-welcome">Welcome to Music Events</h1>
+  <h1 className="music-landing-welcome">Welcome to Music Events</h1>
     <div className="music-landing-sign-in-sign-up">
       <h4 className="music-landing-sign-in"><Link to={routes.SIGN_IN}>Sign-In</Link></h4>
       <h4 className="music-landing-sign-up"><Link to={routes.SIGN_UP}>Sign-Up</Link></h4>
